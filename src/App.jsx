1d4d187ed42e414a76931d6fd8e1ad22f41a8b4e
troxeldj <troxeldj@mail.uc.edu>
2025-04-25 12:41:30 -0400

    add button type to prevent state refresh

--- src/App.jsx
+++ src/App.jsx
@@ -116,6 +116,7 @@ function BattleArea({ selectedGods, onBattleComplete }) {
 
       {!battleOver && (
         <button
+					type="button"
           className="mt-8 px-6 py-3 bg-blue-600 text-white text-lg font-semibold rounded-md shadow-md hover:bg-blue-700 transition"
           onClick={startRound}
         >
